64ffec8b5d628dad1f70153727ba5e3261e68258
SonderUA <umarbek.sonder@gmail.com>
2025-02-21 22:06:52 +0500

    feat(client): update RootLayout to include Sidebar, Providers, and Navigation

--- src/app/layouts/index.tsx
+++ src/app/layouts/index.tsx
@@ -3,6 +3,8 @@ import "@/src/app/styles";
 import { Inter } from "next/font/google";
 import { Roboto } from "next/font/google";
 import { Sidebar } from "@/src/widgets/sidebar";
+import { Providers } from "../provider";
+import { Navigation } from "@/src/shared/ui";
 
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
 const roboto = Roboto({
@@ -29,9 +31,12 @@ export function RootLayout({
 			>
 				<div className="flex h-auto gap-6 p-6 min-h-screen flex-col md:flex-row">
 					<Sidebar />
-					<main className="flex flex-col flex-grow p-5 gap-5 bg-white-500 rounded-2xl min-h-full border border-disabled-100">
-						{children}
-					</main>
+					<Providers>
+						<main className="flex flex-col flex-grow p-5 gap-5 bg-white-500 rounded-2xl min-h-full border border-disabled-100">
+							<Navigation />
+							{children}
+						</main>
+					</Providers>
 				</div>
 			</body>
 		</html>
