3b9fc5091a8ff38d883940415cd6c77bfbf2d2cb
SonderUA <umarbek.sonder@gmail.com>
2025-02-21 19:34:19 +0500

    feat(client): update RootLayout to include Sidebar and main content wrapper

--- src/app/layouts/index.tsx
+++ src/app/layouts/index.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/src/app/styles";
 import { Inter } from "next/font/google";
 import { Roboto } from "next/font/google";
+import { Sidebar } from "@/src/widgets/sidebar";
 
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
 const roboto = Roboto({
@@ -26,7 +27,12 @@ export function RootLayout({
 			<body
 				className={`${inter.variable} ${roboto.variable} antialiased bg-weak-100`}
 			>
-				{children}
+				<div className="flex h-auto gap-6 p-6 min-h-screen flex-col md:flex-row">
+					<Sidebar />
+					<main className="flex flex-col flex-grow p-5 gap-5 bg-white-500 rounded-2xl min-h-full border border-disabled-100">
+						{children}
+					</main>
+				</div>
 			</body>
 		</html>
 	);
