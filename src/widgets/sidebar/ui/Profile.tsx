40f22e796004119c726a067bc27e3d774ab30290
SonderUA <umarbek.sonder@gmail.com>
2025-02-24 20:38:59 +0500

    feat(client): remove dropdown button and update responsive design

--- src/widgets/sidebar/ui/Profile.tsx
+++ src/widgets/sidebar/ui/Profile.tsx
@@ -9,22 +9,13 @@ interface ProfileProps {
 
 export const Profile = ({ username, tokens, plan, img }: ProfileProps) => {
 	return (
-		<section className="flex flex-col p-2.5 rounded-lg gap-2.5 bg-primary-gradient w-fit">
-			<div className="flex justify-between items-center">
-				<div className="flex gap-2.5 items-center">
-					{img}
-					<div className="flex flex-col text-white-500">
-						<h3 className="text-lg font-bold">{username}</h3>
-						<p className="text-xs -mt-1 capitalize">{plan}</p>
-					</div>
+		<section className="flex max-lg-tablet:justify-between max-lg-tablet:w-full lg-tablet:flex-col p-2.5 rounded-lg gap-2.5 bg-primary-gradient w-fit">
+			<div className="flex gap-2.5 items-center">
+				{img}
+				<div className="flex flex-col text-white-500">
+					<h3 className="text-lg font-bold">{username}</h3>
+					<p className="text-xs -mt-1 capitalize">{plan}</p>
 				</div>
-				<button aria-label="Toggle dropdown" className="sm:hidden">
-					<img
-						src="/sidebar/down-icon.svg"
-						alt="a dropdown"
-						className="w-6 h-6"
-					/>
-				</button>
 			</div>
 			<div className="flex items-center gap-2.5">
 				<div className="flex items-center gap-0.5 flex-shrink-0">
@@ -33,15 +24,15 @@ export const Profile = ({ username, tokens, plan, img }: ProfileProps) => {
 						alt="tokens"
 						className="w-5 h-5"
 					/>
-					<p className="text-white-500 text-xs tracking-wide">
+					<p className="text-white-500 text-sm lg-tablet:text-xs tracking-wide">
 						Tokens {tokens}/45
 					</p>
 				</div>
 				<Link
 					href="/premium-plans"
-					className="py-1 px-1.5 rounded-full bg-white-500 text-xs"
+					className="py-1.5 px-2 lg-tablet:py-1 lg-tablet:px-1.5 rounded-full bg-white-500 text-xs"
 				>
-					<span className="bg-primary-gradient bg-clip-text text-transparent text-xs">
+					<span className="bg-primary-gradient bg-clip-text text-transparent text-sm lg-tablet:text-xs">
 						Upgrades
 					</span>
 				</Link>
