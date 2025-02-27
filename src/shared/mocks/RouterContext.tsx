f5951fc5eeb0941077ffbf1dd2872b5572cf725d
SonderUA <umarbek.sonder@gmail.com>
2025-02-27 20:33:22 +0500

    chore(client): remove unused functionality

--- src/shared/mocks/RouterContext.tsx
+++
@@ -1,6 +0,0 @@
-import React, { createContext } from "react";
-
-export const RouterContext = createContext({
-	push: (url: string) => console.log("router.push", url),
-	replace: (url: string) => console.log("router.replace", url),
-});
