5858abdbf5992320467cae761f0554a69aae2f67
SonderUA <umarbek.sonder@gmail.com>
2025-02-27 20:42:57 +0500

    fix(server): remove export from authOptions to resolve error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
 
-export const authOptions = {
+const authOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
 		GitHubProvider({
