fcf6d5bac6540e384adf5a8165cd6dc2cd727a2c
SonderUA <umarbek.sonder@gmail.com>
2025-03-13 16:31:48 +0500

    feat(client): add ArticleInput article generation pages component

---
+++ src/pages/article-generation/ui/ArticleInput.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { Textarea, FileInput } from "@/src/shared/ui";
+import { usePathname } from "next/navigation";
+import { useSelector } from "react-redux";
+import { RootState } from "@/src/app/store";
+import { JSX } from "react";
+import React from "react";
+
+const inputMapping: Record<string, JSX.Element> = {
+	prompt: (
+		<Textarea
+			id="textarea"
+			name="textarea"
+			placeholder="Type a prompt ..."
+			className="h-[9.375rem]"
+			required
+		/>
+	),
+	image: (
+		<FileInput
+			id="image"
+			name="file"
+			className="h-20"
+			accept="image/*"
+			required
+		/>
+	),
+	video: (
+		<FileInput
+			id="video"
+			name="file"
+			className="h-20"
+			accept="video/*"
+			required
+		/>
+	),
+	audio: (
+		<FileInput
+			id="audio"
+			name="file"
+			className="h-20"
+			accept="audio/*"
+			required
+		/>
+	),
+	document: (
+		<FileInput
+			id="document"
+			name="file"
+			className="h-20"
+			accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+			required
+		/>
+	),
+};
+
+export default function ArticleInput() {
+	const pathname = usePathname()!;
+	const source = useSelector((state: RootState) =>
+		state.articleForm.selects.at(-1)
+	)!;
+
+	const format = (text: string) => {
+		return `${text.slice(1).split("/")[1]}`;
+	};
+
+	const switchInput = (input: string) => {
+		if (source.value !== "local" && input !== "prompt") {
+			return (
+				<Textarea
+					id="weblink"
+					name="weblink"
+					placeholder="Paste the link here ..."
+					className="h-20"
+					required
+				/>
+			);
+		}
+
+		return inputMapping[input] || null;
+	};
+
+	return <>{switchInput(format(pathname))}</>;
+}
